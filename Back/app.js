adb56726558eff2bf2af4ed36a6b60df10641c42
balestriero <simon.balestriero@gmail.com>
2022-04-07 19:32:17 +0200

    login + signup

--- Back/app.js
+++ Back/app.js
@@ -3,6 +3,19 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 // IMPORTATION ROUTES
 const userRoutes = require("./routes/user");
 const postRoutes = require("./routes/post");
---
+++ Front/index.js
@@ -0,0 +1,63 @@
+const button1 = document.getElementById("btn_signup");
+const button2 = document.getElementById("btn_login");
+
+button1.addEventListener("click", () => {
+  //Récupération des données du formulaire signup
+  let signUsername = document.getElementById("sign_username");
+  let signEmail = document.getElementById("sign_email");
+  let signPassword = document.getElementById("sign_password");
+
+  // Création du signup à envoyer au server
+  const newUser = {
+    username: signUsername.value,
+    email: signEmail.value,
+    password: signPassword.value,
+  };
+  console.log(newUser);
+
+  // Envoi du signup au server
+  fetch("//localhost:3000/api/auth/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newUser),
+  })
+    .then((res) => res.json())
+    .then((value) => {
+      document.location.href = "signup.html";
+    })
+    .catch((err) => {
+      alert("Erreur :" + err.message);
+    });
+  console.log("utilisateur enregistré dans la db");
+});
+
+button2.addEventListener("click", () => {
+  //Récupération des données du formulaire signup
+  let logEmail = document.getElementById("log_email");
+  let logPassword = document.getElementById("log_password");
+
+  // Création du signup à envoyer au server
+  const logUser = {
+    email: logEmail.value,
+    password: logPassword.value,
+  };
+  console.log(logUser);
+
+  // Envoi du signup au server
+  fetch("//localhost:3000/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(logUser),
+  })
+    .then((res) => res.json())
+    .then((value) => {
+      document.location.href = "logged.html";
+    })
+    .catch((err) => {
+      alert("Erreur :" + err.message);
+    });
+});
